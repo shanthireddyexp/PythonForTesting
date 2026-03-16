39261c27559deb558eac8cc067efd5b2d109e272
shanthireddyexp <shanthireddyexp@gmail.com>
2026-03-16 21:52:40 +0530

    Added Day-1 Operators

---
+++ Day-1 Operators.py
@@ -0,0 +1,14 @@
+# Read two numbers from the keyboard and print minimum value
+
+a = int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+min = a if a<b else b
+print ("Minimum value: ",min)
+
+# Read three numbers from the keyboard and print minimum value
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+min = a if a<b else b if b<c else c
+print ("Minimum value: ",min)
