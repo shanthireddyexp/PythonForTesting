7a67bcafb41ab2dcd7526f75f20a1e39d8cb025c
shanthireddyexp <shanthireddyexp@gmail.com>
2026-03-22 06:53:23 +0530

    Added examples for numeric datatype

---
+++ Day_01DataTypes/Numeric.py
@@ -0,0 +1,33 @@
+# Basic operations using numeric datatypes
+
+firstNum = int(input("Enter the first number: "))
+secondNum = int(input("Enter the second number: "))
+
+# Addition
+print("Sum of two numbers: ", firstNum + secondNum)
+
+# Multiplication
+print("Multiplication of two numbers: ", firstNum * secondNum)
+
+#Division
+print("Division of two numbers: ", firstNum / secondNum)
+
+#Modulo
+print("Modulo of two numbers: ", firstNum % secondNum)
+
+#Subtraction
+print("Subtraction of two numbers: ", firstNum - secondNum)
+
+#Floor Division
+print("Floor division of two numbers: ", firstNum // secondNum)
+
+#Exponent Operator
+print("Exponent of two numbers: ", firstNum ** secondNum)
+
+#Absolute Value
+print("Absolute value of first number: ", abs(firstNum))
+print("Absolute value of second number: ", abs(secondNum))
+
+#Rounding
+print("Rounding of first number: ", round(firstNum, 2))
+print("Rounding of second number: ", round(secondNum, 2))
